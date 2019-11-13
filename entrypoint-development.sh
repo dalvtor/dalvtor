497be5ac3471306881fc887c61785432afd28de6
Danny <dalvtor@gmail.com>
2019-11-13 19:40:30 +0000

    Adds wait for it

--- entrypoint-development.sh
+++ entrypoint-development.sh
@@ -1,2 +1,3 @@
+bash ./wait-for-it.sh --timeout=30 ${POSTGRES_HOST}:${POSTGRES_PORT}
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
--- entrypoint-production.sh
+++ entrypoint-production.sh
@@ -1,3 +1,4 @@
+bash ./wait-for-it.sh --timeout=30 ${POSTGRES_HOST}:${POSTGRES_PORT}
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn --workers=5 --bind 0.0.0.0:8000 dalvtor.wsgi
